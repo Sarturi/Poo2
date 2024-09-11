5cfcd9bc4572e64d534a363659a95013bc1a6132
Sarturi <127155383+Sarturi@users.noreply.github.com>
2024-09-11 19:47:53 -0300

    Add files via upload

---
+++ JPA/AtividadeTarefaJPA-main/out/production/AtividadeTarefaJPA-main/br/edu/univille/poo/JPA/Entity/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tarefa (titulo, descricao_longa, finalizado, data_prevista_finalizacao, data_finalizacao)
+VALUES ('Estudar Java', 'aprender java', false, '2024-09-15', null);
+
+INSERT INTO tarefa (titulo, descricao_longa, finalizado, data_prevista_finalizacao, data_finalizacao)
+VALUES ('Desenvolver API', 'Criar API', false, '2024-09-20', null);
+
+INSERT INTO tarefa (titulo, descricao_longa, finalizado, data_prevista_finalizacao, data_finalizacao)
+VALUES ('Terminar Projeto', 'Terminar projeto', true, '2024-09-05', '2024-09-06');
+
+INSERT INTO tarefa (titulo, descricao_longa, finalizado, data_prevista_finalizacao, data_finalizacao)
+VALUES ('Enviar Relatório', 'Enviar relatorio', false, '2024-08-31', null);
